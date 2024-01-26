c693edbad17c4f32c21e32fbca726b503b6f3e79
khahhan <khanhanbui1010@gmail.com>
2024-01-26 14:55:21 +0700

    skeleton

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,74 +1,116 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Table } from 'antd';
-const columns = [
-   {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-   },
-   {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-   },
-   {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-   },
-   {
-      title: 'Action',
-      dataIndex: '',
-      key: 'x',
-      render: () => <a>Delete</a>,
-   },
-];
-const data = [
-   {
-      key: 1,
-      name: 'John Brown',
-      age: 32,
-      address: 'New York No. 1 Lake Park',
-      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
-   },
-   {
-      key: 2,
-      name: 'Jim Green',
-      age: 42,
-      address: 'London No. 1 Lake Park',
-      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
-   },
-   {
-      key: 3,
-      name: 'Not Expandable',
-      age: 29,
-      address: 'Jiangsu No. 1 Lake Park',
-      description: 'This not expandable',
-   },
-   {
-      key: 4,
-      name: 'Joe Black',
-      age: 32,
-      address: 'Sydney No. 1 Lake Park',
-      description: 'My name is Joe Black, I am 32 years old, living in Sydney No. 1 Lake Park.',
-   },
-];
-const Customers = () => (
-   <Table
-      columns={columns}
-      expandable={{
-         expandedRowRender: (record) => (
-            <p
-               style={{
-                  margin: 0,
-               }}
-            >
-               {record.description}
-            </p>  
-         ),
-         rowExpandable: (record) => record.name !== 'Not Expandable',
-      }}
-      dataSource={data}
-   />
-);
+import axios from 'axios';
+import { Skeleton } from 'antd';
+
+const Customers = () => {
+   const [userList, setUserList] = useState([]);
+   const [loading, setLoading] = useState(false);
+   const columns = [
+      {
+         title: 'STT',
+         dataIndex: 'stt',
+         key: 'stt',
+      },
+      {
+         title: 'Họ tên đệm',
+         dataIndex: 'firstName',
+         key: 'firstName',
+      },
+      {
+         title: 'Tên',
+         dataIndex: 'lastName',
+         key: 'lastName',
+      },
+      {
+         title: 'Địa chỉ',
+         dataIndex: 'address',
+         key: 'address',
+      },
+      {
+         title: 'Ngày sinh',
+         dataIndex: 'birthday',
+         key: 'birthday',
+      },
+      {
+         title: 'Số điện thoại',
+         dataIndex: 'phoneNum',
+         key: 'phoneNum',
+      },
+      {
+         title: 'Giới tính',
+         dataIndex: 'gender',
+         key: 'gender',
+      },
+      {
+         title: 'Tên hiển thị',
+         dataIndex: 'userName',
+         key: 'userName',
+      },
+      {
+         title: 'Email',
+         dataIndex: 'email',
+         key: 'email',
+      },
+      {
+         title: 'Kích hoạt',
+         dataIndex: 'isActive',
+         key: 'isActive',
+         render: (isActive) => (isActive ? 'Đã kích hoạt' : 'Chưa kích hoạt'),
+      },
+      {
+         title: 'Action',
+         dataIndex: '',
+         key: 'x',
+         render: () => <a>Edit</a>,
+      },
+   ];
+   useEffect(() => {
+      const fetchData = async () => {
+         try {
+            const token = localStorage.getItem("token");
+            const response = await axios.get('http://localhost:8080/user', {
+               headers: {
+                  "Authorization": `Bearer ${token}`
+               }
+            });
+            const usersWithStt = response.data._embedded.userses.reverse().map((user, index) => ({
+               ...user,
+               stt: index + 1,
+            }));
+            setUserList(usersWithStt);
+            setLoading(true)
+         } catch (error) {
+            console.error('Error fetching reviews:', error);
+         }
+      };
+      fetchData();
+   }, []);
+   return (
+      <>
+         {
+            loading ? (
+               <Table
+                  columns={columns}
+                  expandable={{
+                     expandedRowRender: (record) => (
+                        <p
+                           style={{
+                              margin: 0,
+                           }}
+                        >
+                           {"Họ và tên: " + record.firstName + " " + record.lastName + " - Địa chỉ: " + record.address + " - Số điện thoại: " + record.phoneNum}
+                        </p>
+                     ),
+                     rowExpandable: (record) => record.name !== 'Not Expandable',
+                  }}
+                  dataSource={userList}
+               />
+            ) : (<Skeleton active />)
+         }
+      </>
+   );
+};
+
+
 export default Customers;
--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,56 +1,100 @@
 import { Table } from 'antd';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Skeleton } from 'antd';
+
 const columns = [
    {
-      title: 'Id',
-      dataIndex: 'id',
+      title: 'STT',
+      dataIndex: 'stt',
    },
    {
-      title: 'Content Rate',
+      title: 'Nội dung',
       dataIndex: 'contentRated',
    },
    {
-      title: 'Star',
+      title: 'Số sao đánh giá',
       dataIndex: 'star',
    },
    {
-      title: 'Product',
-      dataIndex: 'product',
+      title: 'Sản phẩm',
+      dataIndex: 'productName',
    },
    {
-      title: 'User',
-      dataIndex: 'user',
+      title: 'Người dùng',
+      dataIndex: 'userEmail',
    },
 ];
 
 const Review = () => {
    const [reviewList, setReviewList] = useState([]);
+   const [loading, setLoading] = useState(false);
 
    useEffect(() => {
       const fetchData = async () => {
          try {
             const token = localStorage.getItem("token");
-            const response = await axios.get('http://localhost:8080/review', {headers: {
-               "Authorization" :  `Bearer ${token}`
-            }});
-            setReviewList(response.data._embedded.reviews);
+            const response = await axios.get('http://localhost:8080/review', {
+               headers: {
+                  "Authorization": `Bearer ${token}`
+               }
+            });
+            const reviewListWithStt = response.data._embedded.reviews.reverse().map((review, index) => ({
+               ...review,
+               stt: index + 1,
+               userEmail: review._links.user.href,
+               productName: review._links.products.href
+            }));
+            setReviewList(reviewListWithStt);
+
+            const userLinks = response.data._embedded.reviews.map(review => review._links.user.href);
+            const productLinks = response.data._embedded.reviews.map(review => review._links.products.href);
+            const userEmailPromises = userLinks.map(link => axios.get(link, {
+               headers: {
+                  "Authorization": `Bearer ${token}`
+               }
+            }));
+            const userEmails = await Promise.all(userEmailPromises);
+            userEmails.forEach((response, index) => {
+               const userEmail = response.data.email;
+               setReviewList(prevState => prevState.map((review, i) => (i === index ? { ...review, userEmail } : review)));
+            });
+            const productsPromises = productLinks.map(link =>
+               axios.get(link, {
+                  headers: {
+                     "Authorization": `Bearer ${token}`
+                  }
+               }));
+            const productsName = await Promise.all(productsPromises);
+            productsName.forEach((response, index) => {
+               const productName = response.data.productName;
+               setReviewList(prevState => prevState.map((review, i) => (i === index ? { ...review, productName } : review)));
+            });
+            setLoading(true)
          } catch (error) {
-            console.error('Error fetching reviews:', error);
+            console.log('Error fetching reviews:', error);
          }
       };
       fetchData();
-   }, []);   
+   }, []);
+
    return (
-      <div>
-         <div className='mt-4'>
-            <h3 className='mb-4'>Danh sách review</h3>
+      <>
+         {loading ? (
             <div>
-               <Table columns={columns} dataSource={reviewList} />
+               <div className='mt-4'>
+                  <h3 className='mb-4'>Danh sách review</h3>
+                  <div>
+                     <Table columns={columns} dataSource={reviewList} />
+                  </div>
+               </div>
             </div>
-         </div>
-      </div>
-   )
+         ) : (
+            <Skeleton active />
+         )}
+      </>
+   );
+
 }
 
-export default Review
+export default Review;
